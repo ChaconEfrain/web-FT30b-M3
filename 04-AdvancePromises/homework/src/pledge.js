273bd0d11eea97d7264387e7a0232329a22cf632
ChaconEfrain <efrachaga@gmail.com>
2022-10-07 11:47:30 -0500

    Code review corrections and chapter 3 done

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,10 +1,70 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+class $Promise {
+  constructor(executor) {
+    if (typeof executor !== "function") {
+      throw TypeError("The executor is not a function");
+    }
 
+    this._state = "pending";
+    this._handlerGroups = [];
+
+    this._internalResolve = function (someData) {
+      if (this._state === "pending") {
+        this._value = someData;
+        this._state = "fulfilled";
+        this._callHandlers();
+      }
+    };
+
+    this._internalReject = function (myReason) {
+      if (this._state === "pending") {
+        this._value = myReason;
+        this._state = "rejected";
+        this._callHandlers();
+      }
+    };
+
+    const that = this;
+    this.resolve = function (data) {
+      that._internalResolve(data);
+    };
+
+    this.reject = function (data) {
+      that._internalReject(data);
+    };
+
+    executor(this.resolve, this.reject);
+
+    this.then = function (s, e) {
+      if (typeof s !== "function") s = null;
+      if (typeof e !== "function") e = null;
+
+      this._handlerGroups.push({ successCb: s, errorCb: e });
+
+      if (this._state !== "pending") this._callHandlers(this._value);
+    };
+
+    this._callHandlers = function () {
+      while (this._handlerGroups.length) {
+        const currentHandler = this._handlerGroups.shift();
+        if (this._state === "fulfilled") {
+          currentHandler.successCb && currentHandler.successCb(this._value);
+        } else {
+          currentHandler.errorCb && currentHandler.errorCb(this._value);
+        }
+      }
+    };
+
+    this.catch = function (e) {
+      this.then(null, e);
+    };
+  }
+}
 
 module.exports = $Promise;
 /*-------------------------------------------------------
